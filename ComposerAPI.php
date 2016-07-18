da7333032206dcf5a78e546f90157bf546fd3386
Andrej Kabachnik <andrej_ka@gmx.de>
2016-07-18 09:45:12 +0200

    Added most commands

--- ComposerAPI.php
+++ ComposerAPI.php
@@ -107,7 +107,7 @@ class ComposerAPI {
 		return $output;
 	}
 	
-	protected function call_command($command, array $arguments = null, array $options = null, OutputInterface $output = null){
+	protected function call_command($command, array $arguments = null, OutputInterface $output = null){
 		if (is_null($output)){
 			$output = $this->get_default_output_formatter();
 		}
@@ -126,7 +126,11 @@ class ComposerAPI {
 		// Get composer
 		$application = $this->get_composer_application();
 		// Call command
-		$code = $application->run(new ArrayInput(array('command' => $command)), $output);
+		$input_array = array('command' => $command);
+		if (!is_null($arguments)){
+			$input_array += $arguments;
+		}
+		$code = $application->run(new ArrayInput($input_array), $output);
 		
 		// Restore environment
 		chdir($dir);
@@ -139,19 +143,234 @@ class ComposerAPI {
 	
 	/**
 	 * 
+	 * @param array|string $package_names
+	 * @return array
+	 */
+	protected function prepare_array_input_args(array $arguments = null){
+		$result = null;
+		if (is_null($arguments) || count($arguments) == 0){
+			return null;
+		} else {
+			foreach ($arguments as $arg){
+				$result[$arg] = true;
+			}
+		}
+		return $result;
+	}
+	
+	/**
+	 * Reads the composer.json file from the current directory, resolves the dependencies, and installs them: see https://getcomposer.org/doc/03-cli.md#install
+	 * 
 	 * @param OutputInterface $output
 	 * @return \Symfony\Component\Console\Output\OutputInterface
 	 */
-	public function install(OutputInterface $output = null) {
-		return $this->call_command('install', null, null, $output);
+	public function install(array $options = null, OutputInterface $output = null) {
+		return $this->call_command('install', $this->prepare_array_input_args($options), $output);
 	}
 	
-	public function update(OutputInterface $output = null) {
-		return $this->call_command('update', null, null, $output);
+	/**
+	 * Updates dependencies to the latest possible version and updates the composer.lock file: see https://getcomposer.org/doc/03-cli.md#update
+	 * E.g. $composer->update() will update all dependencies while $composer->update(array('symfony/css-crawler'), array('--optimize-autoloader')) 
+	 * will update only the CSS crawler symfony component optimizing the autoloader afterwards.
+	 * 
+	 * @param OutputInterface $output
+	 * @return \Symfony\Component\Console\Output\OutputInterface
+	 */
+	public function update(array $package_names = null, array $options = null, OutputInterface $output = null) {
+		$arguments = array();
+		
+		if (is_array($package_names) && count($package_names) > 0){
+			$arguments['packages'] = $package_names;
+		}
+		
+		if ($options = $this->prepare_array_input_args($options)){
+			$arguments += $options;
+		}
+		
+		return $this->call_command('update', $arguments, $output);
 	}
 	
-	public function show(OutputInterface $output = null) {
-		return $this->call_command('show', null, null, $output);
+	/**
+	 * Installes one or more packages: see https://getcomposer.org/doc/03-cli.md#require.
+	 * E.g. $composer->require(array('monolog/monolog:~1.16', 'slim/slim')) will install monolog in version 1.16
+	 * or later and the Slim framework in the latest available version.
+	 * 
+	 * @param array $package_names
+	 * @param array $options
+	 * @param OutputInterface $output
+	 * @return \Symfony\Component\Console\Output\OutputInterface
+	 */
+	public function require(array $package_names, array $options = null, OutputInterface $output = null){
+		$arguments = array();
+		
+		if (is_array($package_names) && count($package_names) > 0){
+			$arguments['packages'] = $package_names;
+		}
+				
+		if ($options = $this->prepare_array_input_args($options)){
+			$arguments += $options;
+		}
+		
+		return $this->call_command('require', $arguments, $output);
+	}
+	
+	/**
+	 * Uninstalles one or more packages: see https://getcomposer.org/doc/03-cli.md#remove.
+	 * E.g. $composer->remove(array('monolog/monolog', 'slim/slim')) will uninstall monolog in version 1.16
+	 * or later and the Slim framework in the latest available version.
+	 * 
+	 * @param array $package_names
+	 * @param array $options
+	 * @param OutputInterface $output
+	 * @return \Symfony\Component\Console\Output\OutputInterface
+	 */
+	public function remove(array $package_names, array $options = null, OutputInterface $output = null){
+		$arguments = array();
+	
+		if (is_array($package_names) && count($package_names) > 0){
+			$arguments['packages'] = $package_names;
+		}
+	
+		if ($options = $this->prepare_array_input_args($options)){
+			$arguments += $options;
+		}
+	
+		return $this->call_command('remove', $arguments, $output);
+	}
+	
+	/**
+	 * Search through the current project's package repositories: see https://getcomposer.org/doc/03-cli.md#search
+	 * 
+	 * @param array $search_terms
+	 * @param array $options
+	 * @param OutputInterface $output
+	 * @return \Symfony\Component\Console\Output\OutputInterface
+	 */
+	public function search(array $search_terms, array $options = null, OutputInterface $output = null) {
+		$arguments = array();
+	
+		if (is_array($search_terms) && count($search_terms) > 0){
+			$arguments['packages'] = $search_terms;
+		}
+	
+		if ($options = $this->prepare_array_input_args($options)){
+			$arguments += $options;
+		}
+	
+		return $this->call_command('search', $arguments, $output);
+	}
+	
+	/**
+	 * Lists all installed packages: see https://getcomposer.org/doc/03-cli.md#show.
+	 * E.g. $composer->show(array('--latest')) will show all packages with the respective latest version available
+	 *
+	 * @param array $options
+	 * @param OutputInterface $output
+	 * @return \Symfony\Component\Console\Output\OutputInterface
+	 */
+	public function show(array $options = null, OutputInterface $output = null) {
+		return $this->call_command('show', $this->prepare_array_input_args($options), $output);
+	}
+	
+	/**
+	 * Shows a list of installed packages that have updates available, including their current and latest versions: see https://getcomposer.org/doc/03-cli.md#outdated
+	 * E.g. $composer->outdated() will list only outdated packages while $composer->outdated(array('--all')) will list alle packages
+	 * installed with the respective latest versions.
+	 * 
+	 * @param array $options
+	 * @param OutputInterface $output
+	 * @return \Symfony\Component\Console\Output\OutputInterface
+	 */
+	public function outdated(array $options = null, OutputInterface $output = null) {
+		return $this->call_command('outdated', $this->prepare_array_input_args($options), $output);
+	}
+	
+	/**
+	 * Lists all packages suggested by currently installed set of packages: see https://getcomposer.org/doc/03-cli.md#suggests.
+	 * E.g. $composer->suggests(array('monolog/monolog', 'slim/slim'), array('--by-package')) will show packages
+	 * suggested by monolog and Slim grouped by package 
+	 * 
+	 * @param array $package_names
+	 * @param array $options
+	 * @param OutputInterface $output
+	 * @return \Symfony\Component\Console\Output\OutputInterface
+	 */
+	public function suggests(array $package_names = null, array $options = null, OutputInterface $output = null){
+		$arguments = array();
+	
+		if (is_array($package_names) && count($package_names) > 0){
+			$arguments['packages'] = $package_names;
+		}
+	
+		if ($options = $this->prepare_array_input_args($options)){
+			$arguments += $options;
+		}
+	
+		return $this->call_command('suggests', $arguments, $output);
+	}
+	
+	/**
+	 * Tells you which other packages depend on a certain package: see https://getcomposer.org/doc/03-cli.md#depends.
+	 * E.g. $composer->depends('doctrine/lexer', array('--tree')) will show packages, that require the doctrine lexer
+	 * with all subrequirements in the form of a tree.
+	 * 
+	 * @param string $package_name
+	 * @param array $options
+	 * @param OutputInterface $output
+	 * @return \Symfony\Component\Console\Output\OutputInterface
+	 */
+	public function depends($package_name, $version = null, array $options = null, OutputInterface $output = null){
+		$arguments = array();
+	
+		$arguments['package'] = $package_name;
+		
+		if (!is_null($version)){
+			$arguments['constraint'] = $version;
+		}
+	
+		if ($options = $this->prepare_array_input_args($options)){
+			$arguments += $options;
+		}
+	
+		return $this->call_command('depends', $arguments, $output);
+	}
+	
+	/**
+	 * Tells you which packages are blocking a given package from being installed: see https://getcomposer.org/doc/03-cli.md#prohibits.
+	 * E.g. $composer->prohibits('symfony/symfony', '3.1', array('--tree')) will show packages in your current project,
+	 * that would prevent the installation of Symfony 3.1 with all their subrequirements in the form of a tree.
+	 *
+	 * @param string $package_name
+	 * @param string $verion
+	 * @param array $options
+	 * @param OutputInterface $output
+	 * @return \Symfony\Component\Console\Output\OutputInterface
+	 */
+	public function prohibits($package_name, $version = null, array $options = null, OutputInterface $output = null){
+		$arguments = array();
+	
+		$arguments['package'] = $package_name;
+		
+		if (!is_null($version)){
+			$arguments['constraint'] = $version;
+		}
+	
+		if ($options = $this->prepare_array_input_args($options)){
+			$arguments += $options;
+		}
+	
+		return $this->call_command('prohibits', $arguments, $output);
+	}
+	
+	/**
+	 * Validates the composer.json file: see https://getcomposer.org/doc/03-cli.md#validate
+	 *
+	 * @param array $options
+	 * @param OutputInterface $output
+	 * @return \Symfony\Component\Console\Output\OutputInterface
+	 */
+	public function validate(array $options = null, OutputInterface $output = null) {
+		return $this->call_command('validate', $this->prepare_array_input_args($options), $output);
 	}
 
 }
